71b0f480d8096d3aea9b3710333165b0399e6612
Tejas SP <tejasprasad2003@gmail.com>
2020-12-05 14:23:12 +0530

    changed coddeeee

---
+++ page2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="test.css">
+    <title>Versant</title>
+</head>
+<body>
+    <div class="container">
+        <div class="navbar">
+            VERSANT MOCK TEST
+        </div>
+        <div class="main_content">
+            <div class="box">
+                <div class="instruction">
+                    Ready to begin?
+                </div>
+
+                <div class="sub_content">
+                    <div class="paragraph">
+                        Follow the on-screen instructions to proceed through the test
+                    </div>
+                    <div class="timer">
+
+                    </div>
+                </div>
+            </div>
+    
+            <form>
+
+            </form>
+
+            
+        </div>
+</body>
+</html>
---
+++ testnumber.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="testnumber.css" />
+    <title>Versant</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="navbar">VERSANT MOCK TEST</div>
+      <div class="main_content">
+        <div class="instruction">Welcome!</div>
+        <div class="sub_content" style="">
+          <div class="content">
+            <h2 style="margin-bottom: 10px; color: gray">
+              Enter your Test Identification Number (TIN) to start a new test or
+              to resume a test that you started earlier.
+            </h2>
+            <br />
+            <h3 style="color: gray">
+              Your TIN is an eight digit code that will be supplied by your test
+              organizer. Enter it into the field on the right to begin taking
+              your test.
+            </h3>
+            <br />
+            <h4 style="color: gray">
+              Example TIN
+              <span style="color: black">1234 5678</span>
+            </h4>
+        </div>
+            <div class="tininput">
+        <input type="text" placeholder="Enter TIN"></input>
+
+            </div>
+         
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
