e5fb503c3bb28536eb1205df15e972e97ee07b6c
Sid2202 <69642512+Sid2202@users.noreply.github.com>
2020-12-05 23:39:26 +0530

    hear and speak page

---
+++ Repeat.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Repeat.css">
+    <title>Versant</title>
+</head>
+<body>
+    <div class="navbar">
+        VERSANT MOCK TEST
+    </div>
+    <div class="main_content">
+        <div class="heading">
+            <div class="instruction">
+                <div class="aicon">
+                    B
+                </div>
+                Repeat
+            </div>
+            <div class="instruction">
+                Instructions
+            </div>
+        </div>
+        
+        <div class="sub_content">
+            <div class="box">
+               
+                <div class="paragraph">
+                    Please repeat each sentence that you hear.
+                </div>
+            </div>
+            <div class="boxes">
+                <div class="head">
+                    <div class="hear">
+                        <h3>You hear:</h3>
+                    </div>
+                    <div class="voice">
+                        <h3>You say:</h3>
+                    </div>
+                </div><div class="content">
+                    <div class="box1">
+                        "Sentence"
+                    </div>
+                    <div class="box2">
+                        "Sentence"
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
