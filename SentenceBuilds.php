0644111d81a41b88307964998c20f0b4567b2d1d
Sid2202 <69642512+Sid2202@users.noreply.github.com>
2020-12-07 23:57:02 +0530

    completed few more pages

---
+++ SentenceBuilds.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="SentenceBuilds.css">
+    <title>Versant</title>
+</head>
+<body>
+    <div class="navbar">
+        VERSANT MOCK TEST
+    </div>
+    <div class="main_content">
+        <div class="heading">
+            <div class="instruction">
+                <div class="aicon">
+                    C
+                </div>
+                Sentence Builds
+            </div>
+        </div>
+        
+        <div class="sub_content">
+            <div class="box">
+                <div class="paragraph">
+                    Please arrange the word groups into sentences.
+                </div>
+            </div>
+            <div class="boxes">
+                <div class="head">
+                    <div class="hear">
+                        <h3>You hear:</h3>
+                    </div>
+                    <div class="voice">
+                        <h3>You say:</h3>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="box1">
+                        "Sentence"
+                    </div>
+                    <div class="box2">
+                        "Sentence"
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
---
+++ SentenceBuilds3.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="SentenceBuilds3.css">
+    <title>VERSANT</title>
+</head>
+<body>
+    <div class="navbar">
+        VERSANT MOCK TEST
+    </div>
+    <div class="main_content">
+        <div class="heading">
+            <div class="instruction">
+                <div class="aicon">
+                    C
+                </div>
+                Sentence Builds
+            </div>
+        </div>
+        
+        <div class="sub_content">
+            <h3>Rarrange the word groups intoa sentence.</h3>
+            <div class="box">
+                <img src="resources/Speech.png" />
+            </div>
+        </div>
+    </div>
+</body>
+</html>
